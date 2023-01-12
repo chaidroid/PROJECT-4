6a9eae48f053907327cecfdebe2c0511242dc84c
chai <chaitanyagupta759@gmail.com>
2023-01-12 16:16:26 +0530

    sorting_posts

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -20,7 +20,7 @@ export const createPost = async (req, res) => {
     await newPost.save();
 
     const post = await Post.find();
-    res.status(201).json(post);
+    res.status(201).json(post).sort({_id:-1});
   } catch (err) {
     res.status(409).json({ message: err.message });
   }
@@ -30,7 +30,7 @@ export const createPost = async (req, res) => {
 export const getFeedPosts = async (req, res) => {
   try {
     const post = await Post.find();
-    res.status(200).json(post);
+    res.status(200).json(post).sort({_id:-1});
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
@@ -39,7 +39,7 @@ export const getFeedPosts = async (req, res) => {
 export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
-    const post = await Post.find({ userId });
+    const post = await Post.find({ userId }).sort({_id:-1});
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
