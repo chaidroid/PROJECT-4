f3a3e5c48ae19205aecc2aeae803d93cc7c5bb6e
chai <chaitanyagupta759@gmail.com>
2023-02-11 17:40:57 +0530

    removed _sort_from_feedpost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,7 +29,7 @@ export const createPost = async (req, res) => {
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find().sort({_id:-1});
+    const post = await Post.find();
     res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
