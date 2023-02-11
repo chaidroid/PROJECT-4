e1e9f3666552fbbb3f17d323b4b7a2fc8545e392
chai <chaitanyagupta759@gmail.com>
2023-02-11 17:32:56 +0530

    sort_place_change

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -19,8 +19,8 @@ export const createPost = async (req, res) => {
     });
     await newPost.save();
 //hi
-    const post = await Post.find();
-    res.status(201).json(post).sort({_id:-1});
+    const post = await Post.find().sort({_id:-1});
+    res.status(201).json(post);
   } catch (err) {
     res.status(409).json({ message: err.message });
   }
@@ -29,8 +29,8 @@ export const createPost = async (req, res) => {
 /* READ */
 export const getFeedPosts = async (req, res) => {
   try {
-    const post = await Post.find();
-    res.status(200).json(post).sort({_id:-1});
+    const post = await Post.find().sort({_id:-1});
+    res.status(200).json(post);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
